bd19127f7298f12312a85c4c17e3ae1839be8479
Tom Keddie <github@bronwenandtom.com>
2019-11-15 06:26:40 -0800

    pybadge: add python script

---
+++ pybadge/main.py
@@ -0,0 +1,27 @@
+# https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
+import board
+import displayio
+
+display = board.DISPLAY
+
+# Open the file
+with open("/qrcode.bmp", "rb") as bitmap_file:
+
+    # Setup the file as the bitmap data source
+    bitmap = displayio.OnDiskBitmap(bitmap_file)
+
+    # Create a TileGrid to hold the bitmap
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=displayio.ColorConverter())
+
+    # Create a Group to hold the TileGrid
+    group = displayio.Group()
+
+    # Add the TileGrid to the Group
+    group.append(tile_grid)
+
+    # Add the Group to the Display
+    display.show(group)
+
+    # Loop forever so you can enjoy your image
+    while True:
+        pass
